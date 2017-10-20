04e84f1f6e7edd07e5b35d51e6a4e855255a3d28
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-10-20 16:26:32 +0700

    Add menu creation tool

---
+++ CSBigHomework/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBigHomework
+{
+    class Menu
+    {
+        public static void printTitle(string title)
+        {
+            Console.WriteLine(title);
+        }
+        public static int printMenu(List<string> menuItem,ConsoleColor foreColor,ConsoleColor backColor,ConsoleColor highlightColor,ConsoleColor highlightColor2)
+        {
+            Console.WriteLine("Press Key Down and Key Up to select: ");
+            int choose = 0;
+            bool chosen = false;
+            while (!chosen)
+            {
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        choose++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        choose--;
+                        break;
+                    case ConsoleKey.Enter:
+                        chosen = true;
+                        break;
+                }
+                Console.Clear();
+                if (choose == menuItem.Count)
+                    choose = 0;
+                if (choose == -1)
+                    choose = menuItem.Count - 1;
+                for (int i = 0; i < menuItem.Count; i++)
+                {
+                    Console.ForegroundColor = foreColor;
+                    Console.BackgroundColor = backColor;
+                    if (i == choose)
+                    {
+                        Console.ForegroundColor = highlightColor2;
+                        Console.BackgroundColor = highlightColor;
+                        Console.Write(">>>");
+                    }
+                    Console.WriteLine("\t" + menuItem[i]);
+                }
+                Console.ForegroundColor = foreColor;
+                Console.BackgroundColor = backColor;
+            }
+           
+            return choose;
+        }
+    }
+}
--- CSBigHomework/Program.cs
+++ CSBigHomework/Program.cs
@@ -11,16 +11,40 @@ namespace CSBigHomework
         public static List<Laptop> listLaptop;
         static void Main(string[] args)
         {
-
-
-            Laptop.PrintHeading();
             listLaptop = new List<Laptop>();
-            listLaptop.Add(new Laptop("123", "Dell XPS15", DateTime.Parse("1/1/2016"), 200.50d, 10, Country.US));
-            listLaptop.Add(new Laptop("456", "Asus Gaming", DateTime.Parse("12/5/2015"), 500, 20, Country.Taiwan));
-            foreach (Laptop lap in listLaptop)
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            bool isExit = false;
+            Menu.printTitle("\t\tWelcome to Kiet's Laptop Shop\nPlease select:\n");
+            while (!isExit)
             {
-                lap.Output();
+                switch (Menu.printMenu(new List<string> { "Add new Laptop", "Show List", "Delete a laptop", "Buy a laptop", "Exit" }, ConsoleColor.White, ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Yellow))
+                {
+                    case 0:
+                        Laptop laptop = new Laptop();
+                        laptop.Input();
+                        listLaptop.Add(laptop);
+                        break;
+                    case 1:
+                        Laptop.PrintHeading();
+                        foreach(Laptop lap in listLaptop)
+                        {
+                            lap.Output();
+                        }
+                        break;
+                    case 4:
+                        isExit = true;
+                        break;
+                }
             }
+            Laptop.PrintHeading();
+            //listLaptop = new List<Laptop>();
+            //listLaptop.Add(new Laptop("123", "Dell XPS15", DateTime.Parse("1/1/2016"), 200.50d, 10, Country.US));
+            //listLaptop.Add(new Laptop("456", "Asus Gaming", DateTime.Parse("12/5/2015"), 500, 20, Country.Taiwan));
+            //foreach (Laptop lap in listLaptop)
+            //{
+            //    lap.Output();
+            //}
             Console.ReadLine();
         }
     }
