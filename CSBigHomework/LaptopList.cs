5caade4f8236db291216870c27d5d7c89f4c26b1
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-10-27 12:21:39 +0700

    Add R/W methods

---
+++ CSBigHomework/LaptopList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSBigHomework
+{
+    class LaptopList
+    {
+        private List<Laptop> list;
+        public LaptopList()
+        {
+            list = new List<Laptop>();
+        }
+        public void Add(Laptop laptop)
+        {
+            list.Add(laptop);
+        }
+        public void Remove(Laptop lap)
+        {
+            list.Remove(lap);
+        }
+        public Laptop GetLaptop(int index)
+        {
+            return list[index];
+        }
+        public List<Laptop> GetList()
+        {
+            return list;
+        }
+        public void WriteToFile(string dir)
+        {
+            string data = "";
+            foreach(Laptop laptop in list)
+            {
+                data += laptop.Sku + "\t" + laptop.Name + "\t" + laptop.PublishDate.ToShortDateString()  + "\t" + laptop.Price + "\t" + laptop.QualityOnHand + "\t" + laptop.MadeIn + Environment.NewLine;
+            }
+            using(StreamWriter writer =new StreamWriter(dir))
+            {
+                writer.Write(data);
+            }
+        }
+        public void ReadFromFile(string dir)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(dir))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string data = reader.ReadLine();
+                        string[] listToken = data.Split('\t');
+                        list.Add(new Laptop(listToken[0], listToken[1], DateTime.Parse(listToken[2]), double.Parse(listToken[3]), int.Parse(listToken[4]), listToken[5]));
+                       
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- CSBigHomework/Program.cs
+++ CSBigHomework/Program.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CSBigHomework
 {
     class Program
     {
-        public static List<Laptop> listLaptop;
+        public static LaptopList listLaptop;
         static void Main(string[] args)
         {
-            listLaptop = new List<Laptop>();
+            listLaptop = new LaptopList();
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             bool isExit = false;
-            Menu.printTitle("\t\tWelcome to Kiet's Laptop Shop\nPlease select:\n");
+            Menu.printTitle(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory+"\\Title.txt")+"\t\tWelcome to Kiet's Laptop Shop\nPlease select:\n");
             while (!isExit)
             {
-                switch (Menu.printMenu(new List<string> { "Add new Laptop", "Show List", "Delete a laptop", "Buy a laptop", "Exit" }, ConsoleColor.White, ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Yellow))
+                switch (Menu.printMenu(new List<string> { "Add new Laptop", "Show List","Save to file","Load from file", "Delete a laptop", "Buy a laptop", "Exit" }, ConsoleColor.White, ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Yellow))
                 {
                     case 0:
                         Laptop laptop = new Laptop();
@@ -27,17 +28,25 @@ namespace CSBigHomework
                         break;
                     case 1:
                         Laptop.PrintHeading();
-                        foreach(Laptop lap in listLaptop)
+                        foreach(Laptop lap in listLaptop.GetList())
                         {
                             lap.Output();
                         }
                         break;
-                    case 4:
+                    case 2:
+                        Console.WriteLine("Enter directory: ");
+                        listLaptop.WriteToFile(AppDomain.CurrentDomain.BaseDirectory + Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter directory: ");
+                        listLaptop.ReadFromFile(AppDomain.CurrentDomain.BaseDirectory + Console.ReadLine());
+                        break;
+                    case 6:
                         isExit = true;
                         break;
                 }
             }
-            Laptop.PrintHeading();
+            //Laptop.PrintHeading();
             //listLaptop = new List<Laptop>();
             //listLaptop.Add(new Laptop("123", "Dell XPS15", DateTime.Parse("1/1/2016"), 200.50d, 10, Country.US));
             //listLaptop.Add(new Laptop("456", "Asus Gaming", DateTime.Parse("12/5/2015"), 500, 20, Country.Taiwan));
