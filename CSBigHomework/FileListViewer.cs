bcd7b4896c816db2c9e31cb977f6ee0a8ea2659d
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-11-17 09:44:42 +0700

    Big homework phase 1

--- CSBigHomework/FileListViewer.cs
+++ CSBigHomework/FileListViewer.cs
@@ -10,10 +10,18 @@ namespace CSBigHomework
     class FileListViewer
     {
         private List<string> listFile;
+        /// <summary>
+        /// Create file browser in console style
+        /// </summary>
+        /// <param name="directory">File directory</param>
         public FileListViewer(string directory)
         {
             listFile = Directory.EnumerateFiles(directory).ToList<string>();
         }
+        /// <summary>
+        /// Get list of file in the directory
+        /// </summary>
+        /// <returns></returns>
         public string Open()
         {
             return listFile[Menu.printMenu("Open file list", listFile, ConsoleColor.White, ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Yellow)];
